f7006526f9c0917881b63f8dec54750a4ccc699d
Pushkar Dravid <36061658+pushkardravid@users.noreply.github.com>
2026-07-25 05:11:40 -0700

    Match mobile browser chrome to dark theme

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shikhar-spawn-v15";
+const CACHE_NAME = "shikhar-spawn-v16";
 const FILES = ["./","./index.html","./manifest.json","./service-worker.js","./assets/mom.jpg","./assets/dad.jpg","./assets/founders.png","./assets/fallback-dad-childhood.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
